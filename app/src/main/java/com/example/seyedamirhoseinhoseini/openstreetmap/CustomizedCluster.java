a91cf8f16eae3c8e216327a5c4211098d14af890
ahosseini <amirhosein.hosseini76@gmail.com>
2018-12-24 15:31:46 +0330

    cluster hit test changed

--- app/src/main/java/com/example/seyedamirhoseinhoseini/openstreetmap/CustomizedCluster.java
+++ app/src/main/java/com/example/seyedamirhoseinhoseini/openstreetmap/CustomizedCluster.java
@@ -1,33 +1,20 @@
 package com.example.seyedamirhoseinhoseini.openstreetmap;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Point;
-import android.graphics.PorterDuff;
 import android.graphics.Rect;
-import android.util.Log;
-import android.view.Display;
 import android.view.MotionEvent;
-import android.view.View;
-
 
 import org.osmdroid.api.IGeoPoint;
-import org.osmdroid.bonuspack.clustering.MarkerClusterer;
 import org.osmdroid.bonuspack.clustering.RadiusMarkerClusterer;
 import org.osmdroid.bonuspack.clustering.StaticCluster;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.util.PointL;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.Marker;
-
-import java.util.ArrayList;
 
 public class CustomizedCluster extends RadiusMarkerClusterer {
    public CustomizedCluster(Context ctx) {
       super(ctx);
    }
-
+   int i;
    ClickListener mClickListener;
 
 
@@ -36,7 +23,7 @@ public class CustomizedCluster extends RadiusMarkerClusterer {
       boolean touched = hitTest(event, mapView, this);
       if (touched && mapView.getZoomLevel() < 17) {
          if (event.getAction() == MotionEvent.ACTION_DOWN)
-            return mClickListener.OnCLusterCLickListener(this.clusterer(mapView).get(0).getPosition());
+            return mClickListener.OnCLusterCLickListener(this.clusterer(mapView).get(i).getPosition());
       }
       return super.onSingleTapConfirmed(event, mapView);
    }
@@ -44,12 +31,16 @@ public class CustomizedCluster extends RadiusMarkerClusterer {
 
    private boolean hitTest(final MotionEvent event, final org.osmdroid.views.MapView mapView, RadiusMarkerClusterer cluster) {
       Point mPositionPixels = new Point();
-      StaticCluster c = cluster.clusterer(mapView).get(0);
-      mapView.getProjection().toPixels(c.getPosition(), mPositionPixels);
-      final Rect screenRect = mapView.getIntrinsicScreenRect(null);
-      int x = -mPositionPixels.x + screenRect.left + (int) event.getX();
-      int y = -mPositionPixels.y + screenRect.top + (int) event.getY();
-      return cluster.getBounds().contains(x, y);
+      for (i=0; i < cluster.clusterer(mapView).size(); i++) {
+         StaticCluster c = cluster.clusterer(mapView).get(i);
+         mapView.getProjection().toPixels(c.getPosition(), mPositionPixels);
+         final Rect screenRect = mapView.getIntrinsicScreenRect(null);
+         int x = -mPositionPixels.x + screenRect.left + (int) event.getX();
+         int y = -mPositionPixels.y + screenRect.top + (int) event.getY();
+         if(cluster.getBounds().contains(x,y))
+         return true;
+      }
+      return false;
    }
 
    public void setOnClickListener(ClickListener listener) {
